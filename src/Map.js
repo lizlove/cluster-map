8621a6416244c44aca9b108137bab08bea73f7cb
Elizabeth Lovero <elizabethlovero@gmail.com>
2021-11-03 17:55:16 -0400

    Update stub popup functionality in React.

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,7 @@
 import React, { useRef, useEffect, useState } from "react";
+import ReactDOM from "react-dom";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import Popup from "./Popup";
 
 import "mapbox-gl/dist/mapbox-gl.css";
 import "./map.css";
@@ -9,13 +11,15 @@ mapboxgl.accessToken =
 
 export default function Map() {
   const mapContainer = useRef(null);
-  const tooltipRef = useRef(new mapboxgl.Popup({ offset: 15 }));
+  const popupRef = useRef(new mapboxgl.Popup({ offset: 25 }));
   // See: https://github.com/mapbox/mapbox-react-examples/blob/master/react-tooltip/src/Map.js
+
   const map = useRef(null);
   const [lng, setLng] = useState(-96.35);
   const [lat, setLat] = useState(39.5);
   const [zoom, setZoom] = useState(4);
 
+  // Initialize map
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
@@ -26,13 +30,28 @@ export default function Map() {
     });
   });
 
+  // Plot long / lat and zoom in top corner
+  // useEffect(() => {
+  //   if (!map.current) return; // wait for map to initialize
+  //   map.current.on("move", () => {
+  //     setLng(map.current.getCenter().lng.toFixed(4));
+  //     setLat(map.current.getCenter().lat.toFixed(4));
+  //     setZoom(map.current.getZoom().toFixed(2));
+  //   });
+  // });
+
+  // Add markers and popups
   useEffect(() => {
-    if (!map.current) return; // wait for map to initialize
-    map.current.on("move", () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
+    if (!map.current) return;
+    // Create popup node
+    const popupNode = document.createElement("div");
+    ReactDOM.render(<Popup location={location} />, popupNode);
+
+    popupRef.current
+      .setText("Yo Yo YO")
+      .setLngLat([-73.99102684603675, 40.74121894384252])
+      .setDOMContent(popupNode)
+      .addTo(map.current);
   });
 
   return (
